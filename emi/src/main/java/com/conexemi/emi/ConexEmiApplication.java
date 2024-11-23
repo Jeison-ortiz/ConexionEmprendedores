e4afb58c4d2d7acca6e2c648c24dd318276faeb9
TrujiDev <eetgrisales@gmail.com>
2024-11-23 12:36:41 -0500

    first commit

---
+++ emi/src/main/java/com/conexemi/emi/ConexEmiApplication.java
@@ -0,0 +1,13 @@
+package com.conexemi.emi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexEmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexEmiApplication.class, args);
+	}
+
+}
