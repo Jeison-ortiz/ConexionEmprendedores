85dc1722588649ea82d1db2ca3ee84644adda436
TrujiDev <eetgrisales@gmail.com>
2024-11-23 13:42:12 -0500

    Update CategoryController file

--- emi/src/main/java/com/conexemi/emi/controller/CategoryController.java
+++ emi/src/main/java/com/conexemi/emi/controller/CategoryController.java
@@ -1,7 +1,27 @@
 package com.conexemi.emi.controller;
 
+import com.conexemi.emi.model.Category;
+import com.conexemi.emi.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/category")
+import java.util.List;
+
+@RestController()
+@RequestMapping("/category")
 public class CategoryController {
+
+  @Autowired
+  CategoryRepository repository;
+
+  @GetMapping
+  public ResponseEntity<List<Category>> getCategories() {
+    List<Category> categories = repository.findAll();
+    System.out.println(repository.findAll());
+    return new ResponseEntity<>(categories, HttpStatus.OK);
+  }
 }
