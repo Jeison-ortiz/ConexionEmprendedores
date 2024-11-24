fad15d12cb7b49e248a0ba2f80f2a1d611064c2c
Alejo Madrid Giral <alejomadridg@hotmail.com>
2024-11-24 12:33:14 -0500

    feat/service pkg added and Category contructor, getter and setters

--- emi/src/main/java/com/conexemi/emi/model/Category.java
+++ emi/src/main/java/com/conexemi/emi/model/Category.java
@@ -11,4 +11,25 @@ public class Category {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String nameCategory;
+
+  public Category(int id, String nameCategory) {
+    this.id = id;
+    this.nameCategory = nameCategory;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNameCategory() {
+    return nameCategory;
+  }
+
+  public void setNameCategory(String nameCategory) {
+    this.nameCategory = nameCategory;
+  }
 }
---
+++ emi/src/main/java/com/conexemi/emi/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.conexemi.emi.services;
+
+import com.conexemi.emi.model.Category;
+import com.conexemi.emi.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+        public List<Category> findAllCategory(){
+        return categoryRepository.findAll();
+    }
+
+
+}
