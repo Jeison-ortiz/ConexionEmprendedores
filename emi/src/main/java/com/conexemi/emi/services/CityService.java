84ef631846b3d36268168684b4f3db6864d63e5a
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-11-26 17:04:27 -0500

    feature(model): adjust City Service

--- emi/src/main/java/com/conexemi/emi/services/CityService.java
+++ emi/src/main/java/com/conexemi/emi/services/CityService.java
@@ -6,19 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CityService {
 
-  @Autowired
-  CityRepository cityRepository;
+    @Autowired
+    private CityRepository cityRepository;
 
-  public List<City> findAllCities(){
-    return cityRepository.findAll();
-  }
+    public City createCity(City city) {
+        return cityRepository.save(city);
+    }
 
-  public City createCity(City city){
-    return cityRepository.save(city);
-  }
+    public Optional<City> getCityById(Integer idCity) {
+        return cityRepository.findById(idCity);
+    }
+
+    public Optional<City> getCityByName(String nameCity) {
+        return cityRepository.findByCityName(nameCity);
+    }
+
+    public List<City> getAllCities() {
+        return cityRepository.findAll();
+    }
+
 
 }
