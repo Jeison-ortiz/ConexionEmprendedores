f6eca3ba08f669b1c790f7ae70e4e5cf2c438938
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-11-26 16:58:00 -0500

    feature(model): adjust Role repository

--- emi/src/main/java/com/conexemi/emi/repositories/RoleRepository.java
+++ emi/src/main/java/com/conexemi/emi/repositories/RoleRepository.java
@@ -1,7 +1,13 @@
 package com.conexemi.emi.repositories;
 
 import com.conexemi.emi.model.Role;
+import com.conexemi.emi.model.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByNameRole(RoleType nameRole);
+
 }
