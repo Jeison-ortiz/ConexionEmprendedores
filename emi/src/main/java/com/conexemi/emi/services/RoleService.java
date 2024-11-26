e8c0bdab483169bb2fb646df7e8aa5917223ef61
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-11-26 16:58:32 -0500

    feature(model): adjust and create methods Role service

--- emi/src/main/java/com/conexemi/emi/services/RoleService.java
+++ emi/src/main/java/com/conexemi/emi/services/RoleService.java
@@ -1,20 +1,30 @@
 package com.conexemi.emi.services;
 
 import com.conexemi.emi.model.Role;
+import com.conexemi.emi.model.RoleType;
 import com.conexemi.emi.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleService {
 
-  @Autowired
-  RoleRepository roleRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
-  public List<Role> findAllRoles(){
-    return roleRepository.findAll();
-  }
+    public Optional<Role> getRoleById(Integer idRole) {
+        return roleRepository.findById(idRole);
+    }
+
+    public Optional<Role> getRoleByName(RoleType nameRole) {
+        return roleRepository.findByNameRole(nameRole);
+    }
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
 
 }
