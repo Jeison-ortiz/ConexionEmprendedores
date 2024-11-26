53f7f605952582c84b6b32f2c6df103386749799
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-11-26 16:52:34 -0500

    feature(model): adjust Category repository

--- emi/src/main/java/com/conexemi/emi/repositories/CategoryRepository.java
+++ emi/src/main/java/com/conexemi/emi/repositories/CategoryRepository.java
@@ -1,8 +1,13 @@
 package com.conexemi.emi.repositories;
 
 import com.conexemi.emi.model.Category;
+import com.conexemi.emi.model.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Optional<Category> findByNameCategory(CategoryType nameCategory);
 
 }
