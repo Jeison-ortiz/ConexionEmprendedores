24c6173b8d599c3412eb02ead1d465df4c1f9a64
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-11-26 17:02:51 -0500

    feature(model): adjust endpoint City

--- emi/src/main/java/com/conexemi/emi/controller/CityController.java
+++ emi/src/main/java/com/conexemi/emi/controller/CityController.java
@@ -8,25 +8,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/cities")
+@RequestMapping("/city")
 public class CityController {
 
-  @Autowired
-  CityService cityService;
+    @Autowired
+    private CityService cityService;
 
-  @GetMapping
-  public ResponseEntity<List<City>> getAllCities() {
-    List<City> cities = cityService.findAllCities();
-    System.out.println(cities);
-    return new ResponseEntity<>(cities, HttpStatus.OK);
-  }
+    @PostMapping
+    public ResponseEntity<City> createCity(@RequestBody City city) {
+        City saveCity = cityService.createCity(city);
+        return new ResponseEntity<>(saveCity, HttpStatus.CREATED);
+    }
 
-  @PostMapping
-  public ResponseEntity<City> addCity(@RequestBody City city){
-    City saveCity = cityService.createCity(city);
-    return new ResponseEntity<>(saveCity, HttpStatus.CREATED);
-  }
+    @GetMapping("/id/{idCity}")
+    public ResponseEntity<City> getCityById(@PathVariable Integer idCity) {
+        Optional<City> city = cityService.getCityById(idCity);
+        return city.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/name/{nameCity}")
+    public ResponseEntity<City> getCityByName(@PathVariable String nameCity) {
+        Optional<City> city = cityService.getCityByName(nameCity);
+        return city.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<City>> getAllCities() {
+        List<City> cities = cityService.getAllCities();
+        System.out.println(cities);
+        return new ResponseEntity<>(cities, HttpStatus.OK);
+    }
+
 
 }
