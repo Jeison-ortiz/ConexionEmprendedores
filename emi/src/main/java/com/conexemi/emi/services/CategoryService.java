bdd03a95a3eb43252620197d36216ba3accfa3bd
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-11-26 16:53:21 -0500

    feature(model): adjust and create methods Category service

--- emi/src/main/java/com/conexemi/emi/services/CategoryService.java
+++ emi/src/main/java/com/conexemi/emi/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.conexemi.emi.services;
 
 import com.conexemi.emi.model.Category;
+import com.conexemi.emi.model.CategoryType;
 import com.conexemi.emi.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -13,7 +15,15 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public List<Category> findAllCategory(){
+    public Optional<Category> getCategoryById(Integer idCategory) {
+        return categoryRepository.findById(idCategory);
+    }
+
+    public Optional<Category> getCategoryByName(CategoryType nameCategory) {
+        return categoryRepository.findByNameCategory(nameCategory);
+    }
+
+    public List<Category> getAllCategory() {
         return categoryRepository.findAll();
     }
 
