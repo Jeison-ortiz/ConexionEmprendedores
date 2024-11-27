ef5672996f238e28e10497560cb735a6f013b842
FabianaArciniegas <fabianaarciniegasc@gmail.com>
2024-11-27 17:19:15 -0500

    feature(model): creation of updateDTO to use in the update endpoint

---
+++ emi/src/main/java/com/conexemi/emi/DTO/UpdateUserDTO.java
@@ -0,0 +1,25 @@
+package com.conexemi.emi.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UpdateUserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String mobile;
+    private String userPassword;
+    private Integer idCity;
+    private List<Integer> idRoles;
+
+
+}
